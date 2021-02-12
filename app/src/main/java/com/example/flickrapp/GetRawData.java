02b781edbfff3cd5004689aefb263dca5982c442
Levasseur <sebastien.levasseur@efrei.net>
2021-02-12 16:59:39 +0100

    get json from flickr

---
+++ app/src/main/java/com/example/flickrapp/GetRawData.java
@@ -0,0 +1,74 @@
+package com.example.flickrapp;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class GetRawData {
+    private String mRawUrl;
+
+    public GetRawData(String url){
+        this.mRawUrl = url;
+    }
+
+    public void startDownload(){
+        DownloadRawData downloadRawData = new DownloadRawData();
+        downloadRawData.execute(this.mRawUrl);
+    }
+
+    class DownloadRawData extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            Log.d("LINCEO", result);
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+            StringBuffer buffer = null;
+            try {
+                URL url = new URL(params[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream inputStream = urlConnection.getInputStream();
+                if(inputStream == null){
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+                String line;
+                buffer = new StringBuffer();
+                while((line = reader.readLine()) != null){
+                    buffer.append(line + "\n");
+                }
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            finally{
+                if(urlConnection != null){
+                    urlConnection.disconnect();
+                }
+                if(reader != null){
+                    try {
+                        reader.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            return buffer.toString();
+        }
+    }
+}
+
+
+
